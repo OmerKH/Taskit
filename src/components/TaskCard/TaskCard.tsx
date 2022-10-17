ea2a97ca1b91b661f820f293911163b804c1050b
Omer Kirshner <omer.khs@gmail.com>
2022-10-17 14:24:39 +0300

    check

--- src/components/TaskCard/TaskCard.tsx
+++ src/components/TaskCard/TaskCard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Task } from "../../model";
 import { AiFillEdit, AiOutlineDelete } from "react-icons/ai";
 import { MdDone } from "react-icons/md";
@@ -11,6 +11,9 @@ interface Props {
 }
 
 const TaskCard: React.FC<Props> = ({ task, taskList, setTaskList }) => {
+  const [edit, setEdit] = useState<boolean>(false);
+  const [taskEdit, setTaskEdit] = useState<string | number>(task.task);
+
   const handleDone = (id: number) => {
     setTaskList(
       taskList.map((task) =>
@@ -23,16 +26,36 @@ const TaskCard: React.FC<Props> = ({ task, taskList, setTaskList }) => {
     setTaskList(taskList.filter((task) => task.id !== id));
   };
 
+  const handleEdit = (e: React.FormEvent, id: number) => {
+    e.preventDefault();
+
+    setTaskList(
+      taskList.map((todo) =>
+        todo.id === id ? { ...todo, todo: taskEdit } : todo
+      )
+    );
+    setEdit(false);
+  };
+
   return (
-    <div className="task-card">
-      {task.isDone ? (
+    <form className="task-card" onSubmit={(e) => handleEdit(e, task.id)}>
+      {edit ? (
+        <input
+          className="task-test"
+          value={taskEdit}
+          onChange={(e) => setTaskEdit(e.target.value)}
+        />
+      ) : task.isDone ? (
         <s className="task-text">{task.task}</s>
       ) : (
         <span className="task-text">{task.task}</span>
       )}
 
       <div>
-        <span className="icon">
+        <span
+          className="icon"
+          onClick={() => (!edit && !task.isDone ? setEdit(!edit) : edit)}
+        >
           <AiFillEdit />
         </span>
         <span className="icon" onClick={() => handleDelete(task.id)}>
@@ -42,7 +65,7 @@ const TaskCard: React.FC<Props> = ({ task, taskList, setTaskList }) => {
           <MdDone />
         </span>
       </div>
-    </div>
+    </form>
   );
 };
 
